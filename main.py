d93f89ad02523189ec74f7d965990cbd76a92bc4
chronowix <amelia.garcia.fern@gmail.com>
2022-12-15 12:13:45 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-import os
 from pyfiglet import Figlet
 
 # title
 f = Figlet(font='slant')
-print (f.renderText('PyCLI'))
+print(f.renderText('PyCLI'))
 print("Welcome to PyCLI! Select an action from 1 to 4:\n ")
 
 while True:
-    print("1.")
-    print("2.")
-    print("3.")
+    print("1.About me!")
+    print("2.What is PyCLI?")
+    print("3.Opinion on Java")
     print("4.Exit")
 
     choice = input(">>> ")
     choice = choice.strip()
 
     if (choice == "1"):
-        print("hi!!!")
+        print("Chrono, 20, coding enthusiastic, loves drawing, playing games and of course CODING!")
 
-    elif(choice == "2"):
-        print("i'm gay!!!")
+    elif (choice == "2"):
+        print("It's a command-line interface template based on Python, anyone can use the source code!")
 
-    elif(choice == "3"):
-        print("yolo")
+    elif (choice == "3"):
+        print("no.")
 
-    elif(choice == "4"):
+    elif (choice == "4"):
         break
 
     else:
